51f6423f37b44250e6768d25c9fcac886e0c94e8
dukungin <dukung.in.support@gmail.com>
2026-06-12 20:35:34 +0700

    fix

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { showSessionExpiredModal } from './sessionModal';
 
 const api = axios.create({
-  baseURL: 'https://server-ttt-production.up.railway.app' || import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
