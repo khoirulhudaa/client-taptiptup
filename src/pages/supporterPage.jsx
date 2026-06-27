4549c35d193694c0d048ce9a0546369fba1f5201
dukungin <dukung.in.support@gmail.com>
2026-06-27 09:59:13 +0700

    fix

--- src/pages/supporterPage.jsx
+++ src/pages/supporterPage.jsx
@@ -2716,7 +2716,7 @@ const SupporterPage = () => {
               disabled={isSubmitDisabled}
               className={`w-full py-3 md:py-4 rounded-xl font-black text-sm flex items-center justify-center gap-2 transition-all ${
                 isSubmitDisabled
-                  ? 'bg-slate-200 dark:bg-slate-800 text-slate-400 cursor-not-allowed'
+                  ? 'bg-slate-200 dark:bg-slate-700 text-slate-400 cursor-not-allowed'
                   : 'active:scale-[0.99] cursor-pointer bg-blue-600 text-white hover:brightness-110'
               }`}
             >
