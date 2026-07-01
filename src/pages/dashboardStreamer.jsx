4cb5af1cf97f733cbc43d83a4ad59683964f87a9
dukungin <dukung.in.support@gmail.com>
2026-07-01 08:39:00 +0700

    fix

--- src/pages/dashboardStreamer.jsx
+++ src/pages/dashboardStreamer.jsx
@@ -5759,27 +5759,29 @@ const handleChangePin = async () => {
                       </div>
                     </div>
 
-                    <button onClick={() => saveSettingsMutation.mutate({ settings, slot: activeSlot })} disabled={saveSettingsMutation.isPending}
-                      className="
-                      text-slate-900 dark:text-white bg-slate-100 dark:bg-white/20
-                      -translate-y-[3px] translate-x-[-3px]
-                      [box-shadow:4px_6px_0_#f1f5f9]
-                      dark:[box-shadow:4px_4px_0_#99a3b1]
-                      hover:translate-y-0 hover:translate-x-0
-                      hover:bg-slate-200 dark:hover:bg-slate-700
-                      border border-slate-300
-                      hover:[box-shadow:0_0_0_#f1f5f9]
-                      dark:hover:[box-shadow:0_0_0_#94a3b8]
-                      active:translate-y-[2px] active:translate-x-[2px]
-                      active:[box-shadow:none]
-                      active:bg-slate-300 dark:active:bg-slate-800
-                      cursor-pointer active:scale-[0.99] hover:brightness-90 w-full bg-slate-900/70 dark:bg-slate-700 text-white py-3 md:py-4 rounded-xl font-black text-sm transition-all shadow-xl shadow-slate-200 dark:shadow-none disabled:opacity-70 flex items-center justify-center gap-3">
-                      {saveSettingsMutation.isPending ? (
-                        <><RefreshCw size={18} className="animate-spin" /> Menyimpan...</>
-                      ) : (
-                        <> Simpan Sekarang</>
-                      )}
-                    </button>
+                    <div className='w-full px-[4px] mt-5'>
+                      <button onClick={() => saveSettingsMutation.mutate({ settings, slot: activeSlot })} disabled={saveSettingsMutation.isPending}
+                        className="
+                        text-slate-900 dark:text-white bg-slate-100 dark:bg-white/20
+                        -translate-y-[3px] translate-x-[-3px]
+                        [box-shadow:4px_6px_0_#f1f5f9]
+                        dark:[box-shadow:4px_4px_0_#99a3b1]
+                        hover:translate-y-0 hover:translate-x-0
+                        hover:bg-slate-200 dark:hover:bg-slate-700
+                        border border-slate-300
+                        hover:[box-shadow:0_0_0_#f1f5f9]
+                        dark:hover:[box-shadow:0_0_0_#94a3b8]
+                        active:translate-y-[2px] active:translate-x-[2px]
+                        active:[box-shadow:none]
+                        active:bg-slate-300 dark:active:bg-slate-800
+                        cursor-pointer active:scale-[0.99] hover:brightness-90 w-full bg-slate-900/70 dark:bg-slate-700 text-white py-3 md:py-4 rounded-xl font-black text-sm transition-all shadow-xl shadow-slate-200 dark:shadow-none disabled:opacity-70 flex items-center justify-center gap-3">
+                        {saveSettingsMutation.isPending ? (
+                          <><RefreshCw size={18} className="animate-spin" /> Menyimpan...</>
+                        ) : (
+                          <> Simpan Sekarang</>
+                        )}
+                      </button>
+                    </div>
 
                     <div className='w-full flex items-center gap-3'>
                       <div className="w-full mt-4 flex items-center gap-3 bg-slate-100 dark:bg-slate-800 p-4 rounded-xl border-2 border-dashed border-slate-200 dark:border-slate-700">
